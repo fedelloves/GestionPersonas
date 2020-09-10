1946b2d35a8c794388bff343d60cacdf5e077900
Federico Lloves <fede.lloves@gmail.com>
2020-09-10 19:37:03 -0300

    Agrego el controller para Pais

---
+++ src/main/java/com/gestionpersonas/GestionPersonas/controller/PaisController.java
@@ -0,0 +1,25 @@
+package com.gestionpersonas.GestionPersonas.controller;
+
+import com.gestionpersonas.GestionPersonas.domain.Pais;
+import com.gestionpersonas.GestionPersonas.service.PaisService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class PaisController {
+
+    @Autowired
+    private PaisService paisService;
+
+    @GetMapping("/")
+    public String listarPaises(Model model){
+        List<Pais> paises = paisService.obtenerPaises();
+        model.addAttribute("paises", paises);
+        return "index";
+
+    }
+}
--- src/main/java/com/gestionpersonas/GestionPersonas/service/PaisServiceImpl.java
+++ src/main/java/com/gestionpersonas/GestionPersonas/service/PaisServiceImpl.java
@@ -2,14 +2,15 @@ package com.gestionpersonas.GestionPersonas.service;
 
 import com.gestionpersonas.GestionPersonas.domain.Pais;
 import com.gestionpersonas.GestionPersonas.repository.PaisRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Set;
 
 @Service
 public class PaisServiceImpl implements PaisService {
 
+    @Autowired
     private PaisRepository paisRepository;
 
     @Override
