52ef9750db8a597a1ce417fe23907705af338075
Federico Lloves <fede.lloves@gmail.com>
2020-09-10 16:24:26 -0300

    Se agregar las clases Persona y Pais

---
+++ src/main/java/com/gestionpersonas/GestionPersonas/domain/Pais.java
@@ -0,0 +1,25 @@
+package com.gestionpersonas.GestionPersonas.domain;
+
+public class Pais {
+
+    private Integer idPais;
+    private String nombre;
+
+    public Pais(){}
+
+    public Integer getIdPais() {
+        return idPais;
+    }
+
+    public void setIdPais(Integer idPais) {
+        this.idPais = idPais;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
---
+++ src/main/java/com/gestionpersonas/GestionPersonas/domain/Persona.java
@@ -0,0 +1,61 @@
+package com.gestionpersonas.GestionPersonas.domain;
+
+public class Persona {
+
+    private Integer idPersona;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
+    private Pais pais;
+
+    public Persona(){}
+
+    public Integer getIdPersona() {
+        return idPersona;
+    }
+
+    public void setIdPersona(Integer idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public Pais getPais() {
+        return pais;
+    }
+
+    public void setPais(Pais pais) {
+        this.pais = pais;
+    }
+}
