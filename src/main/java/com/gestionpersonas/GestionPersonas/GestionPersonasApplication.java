9d807ea8e3034d6c9c7e78cd9886e8dc52b89294
Federico Lloves <fede.lloves@gmail.com>
2020-09-10 15:45:24 -0300

    Primer commit

---
+++ src/main/java/com/gestionpersonas/GestionPersonas/GestionPersonasApplication.java
@@ -0,0 +1,13 @@
+package com.gestionpersonas.GestionPersonas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionPersonasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionPersonasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestionpersonas/GestionPersonas/GestionPersonasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestionpersonas.GestionPersonas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionPersonasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
