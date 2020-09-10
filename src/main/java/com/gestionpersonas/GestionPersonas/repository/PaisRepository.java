811f95de0ee159d47e476e3a9c44a5d02206c20b
Federico Lloves <fede.lloves@gmail.com>
2020-09-10 17:00:22 -0300

    Agrego repositorio Pais

---
+++ src/main/java/com/gestionpersonas/GestionPersonas/repository/PaisRepository.java
@@ -0,0 +1,9 @@
+package com.gestionpersonas.GestionPersonas.repository;
+
+import com.gestionpersonas.GestionPersonas.domain.Pais;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaisRepository extends CrudRepository<Pais, Integer> {
+}
