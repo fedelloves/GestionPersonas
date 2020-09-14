5d0f828951ff9dcbc12e5506a84eb601bc7dafc7
Federico Lloves <fede.lloves@gmail.com>
2020-09-14 19:40:05 -0300

    Agrego vista para listar las personas

--- src/main/java/com/gestionpersonas/GestionPersonas/controller/PersonaController.java
+++ src/main/java/com/gestionpersonas/GestionPersonas/controller/PersonaController.java
@@ -3,19 +3,22 @@ package com.gestionpersonas.GestionPersonas.controller;
 import com.gestionpersonas.GestionPersonas.domain.Persona;
 import com.gestionpersonas.GestionPersonas.service.PersonaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class PersonaController {
 
     @Autowired
     private PersonaService personaService;
 
-    @GetMapping("/")
-    public List<Persona> listaPersonas(){
-        return personaService.obtenerPersonas();
+    @GetMapping("/personas")
+    public String listaPersonas(Model model){
+        List<Persona> personas = personaService.obtenerPersonas();
+        model.addAttribute("personas", personas);
+        return "personas";
     }
 }
